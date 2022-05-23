443b3cad300be6c23200cda90803c27c25472803
DelowarHossain69 <daloar2400@gmail.com>
2022-05-23 01:39:34 -0700

    product cart design

--- src/components/pages/Home/Products/Products.js
+++ src/components/pages/Home/Products/Products.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Products = () => {
+    const [products, setProducts] = useState([]);
     return (
         <section>
-             
+            <h1></h1>
+
+            <div className='gird grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+                {
+                    products?.map(product => )
+                }
+            </div>
         </section>
     );
 };
