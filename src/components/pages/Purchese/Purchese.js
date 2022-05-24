e76fb1c4b1d3b3a05b11465b81844d38850bc57e
DelowarHossain69 <daloar2400@gmail.com>
2022-05-23 20:51:22 -0700

    load data and show in purchese page

--- src/components/pages/Purchese/Purchese.js
+++ src/components/pages/Purchese/Purchese.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from './../../../firebase.init';
+import Loading from "../../shared/Loading/Loading";
+
 
 const Purchese = () => {
+    const [user, loading] = useAuthState(auth);
     const {id} = useParams();
+    const [product, setProduct] = useState({});
+    const {name, img, description, } = product;
+    
+    useEffect(()=>{
+        const url = `http://localhost:5000/product/${id}?email=${user?.email}`;
+        fetch(url, {
+            headers : {
+                auth : `Bearer ${localStorage.getItem('accessToken')} `
+            }
+        })
+        .then(res => res.json())
+        .then(data => setProduct(data));
+    }, [id, user]);
+
+    if(loading){
+        return <Loading />
+    }
 
   return (
     <section className="grid grid-cols-1 md:grid-cols-2">
@@ -10,16 +32,12 @@ const Purchese = () => {
         <div class="hero min-h-screen bg-base-200">
           <div class="hero-content flex-col lg:flex-row">
             <img
-              src="https://api.lorem.space/image/movie?w=260&h=400"
+              src={img}
               class="max-w-sm rounded-lg shadow-2xl" alt="product pic"
             />
             <div>
-              <h1 class="text-5xl font-bold">Box Office News!</h1>
-              <p class="py-6">
-                Provident cupiditate voluptatem et in. Quaerat fugiat ut
-                assumenda excepturi exercitationem quasi. In deleniti eaque aut
-                repudiandae et a id nisi.
-              </p>
+              <h1 class="text-5xl font-bold">{name}</h1>
+              <p class="py-6">{description}</p>
               <button class="btn btn-primary">Get Started</button>
             </div>
           </div>
