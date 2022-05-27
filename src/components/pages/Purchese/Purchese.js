193c10502c2839e0b9e037320b62e495ffcee450
DelowarHossain69 <daloar2400@gmail.com>
2022-05-27 06:04:58 -0700

    purchese page updated

--- src/components/pages/Purchese/Purchese.js
+++ src/components/pages/Purchese/Purchese.js
@@ -3,7 +3,7 @@ import { useNavigate, useParams } from "react-router-dom";
 import { useAuthState } from "react-firebase-hooks/auth";
 import auth from "./../../../firebase.init";
 import Loading from "../../shared/Loading/Loading";
-import { toast } from 'react-toastify';
+import { toast } from "react-toastify";
 
 const Purchese = () => {
   const navigate = useNavigate();
@@ -26,57 +26,50 @@ const Purchese = () => {
       .then((data) => setProduct(data));
   }, [id, user]);
 
-  const handleQuantity = (e) => {
-    const quantity = e.target.value;
-    setQuantity(quantity);
-  };
-  
   // Calculate order summery
-  const productQuantity  =  quantity || minQuantity;
-  const subTotal = price *  productQuantity;
+  const productQuantity = quantity || minQuantity;
+  const subTotal = price * productQuantity;
   const shipping = 5 * productQuantity;
   const total = subTotal + shipping;
 
-
-  const handlePlaceOrder = e =>{
+  const handlePlaceOrder = (e) => {
     e.preventDefault();
     const phone = e.target.phone.value;
     const shippingAddress = e.target.shippingAddress.value;
 
     const orderInfo = {
-      productName : name,
-      quantity : productQuantity,
-      productId : _id,
-      paid : false,
-      email : user?.email,
-      customer : user?.displayName,
-      status : 'Pending',
-      price : total,
+      productName: name,
+      quantity: productQuantity,
+      productId: _id,
+      paid: false,
+      email: user?.email,
+      customer: user?.displayName,
+      status: "Pending",
+      price: total,
       phone,
       shippingAddress,
-      productImg : img,
-    }
+      productImg: img,
+    };
 
-    if(_id){
+    if (_id) {
       fetch(`http://localhost:5000/order?email=${user?.email}`, {
-        method : "POST",
+        method: "POST",
         headers: {
-          'content-type' : 'application/json',
-          auth : `Bearer ${localStorage.getItem('accessToken')}`
+          "content-type": "application/json",
+          auth: `Bearer ${localStorage.getItem("accessToken")}`,
         },
-        body: JSON.stringify(orderInfo)
+        body: JSON.stringify(orderInfo),
       })
-      .then(res => res.json())
-      .then(data => {
-        if(data.insertedId){
-          toast.success('Your order have been complete. Continue shopping');
-          navigate('/products');
-        }
-      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.insertedId) {
+            toast.success("Your order have been complete. Continue shopping");
+            navigate("/products");
+          }
+        });
     }
+  };
 
-  }
-  
   if (loading) {
     return <Loading />;
   }
@@ -87,11 +80,7 @@ const Purchese = () => {
         <div class="hero min-h-screen bg-[#dddddd29] rounded shadow-xl">
           <div class="hero-content flex-col lg:flex-row space-x-5 items-start">
             <div className="lg:w-1/2">
-              <img
-                src={img}
-                class="max-w-full rounded-lg "
-                alt="product pic"
-              />
+              <img src={img} class="max-w-full rounded-lg " alt="product pic" />
             </div>
             <div className="lg:w-1/2 space-y-5">
               <h1 class="text-3xl mt-8 lg:mt-0">{name}</h1>
@@ -106,19 +95,6 @@ const Purchese = () => {
                 Min order quantity : <strong>{minQuantity} peace</strong>
               </h2>
 
-              <form action="" onChange={handleQuantity}>
-                <p>
-                  Quantity :
-                  <input
-                    type="number"
-                    placeholder="Quantity"
-                    class="input input-bordered w-28 ml-3"
-                    min={minQuantity}
-                    value={quantity}
-                  />
-                </p>
-              </form>
-
               <div>
                 <h2 className="text-xl font-bold mt-8 mb-3">Order Summary</h2>
                 <table>
@@ -128,7 +104,7 @@ const Purchese = () => {
                   </tr>
                   <tr>
                     <td>Shipping Fee</td>
-                    <td>: $  {shipping}</td>
+                    <td>: $ {shipping}</td>
                   </tr>
                   <tr>
                     <td>Total: </td>
@@ -146,37 +122,76 @@ const Purchese = () => {
       </div>
 
       <div className="lg:col-span-2">
-        
-        <form action="" className="space-y-5 w-full  md:w-4/5 lg:max-w-full lg:w-full mx-auto shadow-lg p-5 rounded" onSubmit={handlePlaceOrder}>
-        <h2 className="text-xl mb-3 font-bold">Shipping & Billing</h2>
+        <form
+          action=""
+          className="space-y-5 w-full  md:w-4/5 lg:max-w-full lg:w-full mx-auto shadow-lg p-5 rounded"
+          onSubmit={handlePlaceOrder}
+        >
+          <h2 className="text-xl mb-3 font-bold">Shipping & Billing</h2>
           <div class="form-control w-full">
             <label class="label">
               <span class="label-text  text-lg">Your name</span>
             </label>
-            <input type="text" class="input input-bordered w-full text-lg" value={user?.displayName}  disabled/>
+            <input
+              type="text"
+              class="input input-bordered w-full text-lg"
+              value={user?.displayName}
+              disabled
+            />
           </div>
 
           <div class="form-control w-full">
             <label class="label">
               <span class="label-text  text-lg">Your Email</span>
             </label>
-            <input type="text" class="input input-bordered w-full text-lg" value={user?.email}  disabled/>
+            <input
+              type="text"
+              class="input input-bordered w-full text-lg"
+              value={user?.email}
+              disabled
+            />
+          </div>
+
+          <div class="form-control w-full">
+            <label class="label">
+              <span class="label-text  text-lg">Product Quantity</span>
+            </label>
+            <input
+              type="number"
+              placeholder="Product quantity"
+              class="input input-bordered w-full text-lg"
+              onChange={(e) => setQuantity(e.target.value)}
+              min={minQuantity}
+              max={maxQuantity}
+            />
           </div>
 
           <div class="form-control w-full">
             <label class="label">
               <span class="label-text  text-lg">Phone*</span>
             </label>
-            <input type="number" name='phone' class="input input-bordered w-full text-lg" required />
+            <input
+              type="number"
+              name="phone"
+              placeholder="Phone"
+              class="input input-bordered w-full text-lg"
+              required
+            />
           </div>
 
           <div class="form-control w-full">
             <label class="label">
               <span class="label-text  text-lg">Shipping address*</span>
             </label>
-            <input type="text" name='shippingAddress' class="input input-bordered w-full text-lg" required placeholder="Your address"/>
+            <input
+              type="text"
+              name="shippingAddress"
+              class="input input-bordered w-full text-lg"
+              required
+              placeholder="Your address"
+            />
           </div>
-          
+
           <button className="btn btn-secondary w-full">Place order</button>
         </form>
       </div>
